3e8fe5b35e16b87f30ddf73fe011987dc230507d
윤성호 <youn2029@naver.com>
2019-02-19 20:04:28 +0900

    클래스에 UITableViewDelegate와 UITableViewDataSource 프로토콜 선언

--- MyMovieChart-SpinOff/ViewController.swift
+++ MyMovieChart-SpinOff/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // 테이블 뷰 셀의 갯수를 설정
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // 테이블 뷰 셀의 구성 설정
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "\(indexPath.row)번째 데이터입니다"
+        return cell
+    }
+    
+    // 테이블 뷰 셀을 클릭했을 때 호출
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLos("\(indexPath.row)번째 데이터 클릭")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
